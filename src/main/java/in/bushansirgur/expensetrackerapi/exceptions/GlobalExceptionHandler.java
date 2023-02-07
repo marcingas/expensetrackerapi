7ef5aed286e64e5cc3e0af7a57d82feb6d74452a
Marcin <gasiorek.mail@gmail.com>
2023-02-07 12:27:57 +0100

    new functionality handling internal server error

--- src/main/java/in/bushansirgur/expensetrackerapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/in/bushansirgur/expensetrackerapi/exceptions/GlobalExceptionHandler.java
@@ -31,4 +31,13 @@ return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
         errorObject.setTimestamp(new Date());
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleGeneralException(Exception ex, WebRequest request){
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
